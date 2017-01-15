be1090560cc1b793ac1f608cbb287cc8e91c8f02
akokshar <akokshar@redhat.com>
2017-01-15 14:36:45 +0100

    added 'command' option

--- termlex.c
+++ termlex.c
@@ -53,15 +53,53 @@ gboolean window_delete_event_callback(GtkWidget *widget, GdkEvent *event, GPid *
 
 int main (int argc, char *argv[]) {
 	
-	GtkWindow *window = NULL;
-	GtkBox *box = NULL;
-	GtkScrollbar* scrollbar;
-	VteTerminal *vte = NULL;
-
-	GPid child_pid = 0;
 	GError *error = NULL;
 
 	gtk_init(&argc, &argv);
+
+	gchar *command = NULL;
+	const GOptionEntry entries[] = {
+		{"command", 'c', 0, G_OPTION_ARG_STRING, &command, "Command to execute. Defaults to user's shell", "COMMAND"},
+		{ NULL }
+	};
+
+	GOptionContext *option_context = g_option_context_new(NULL);
+	g_option_context_set_help_enabled(option_context, TRUE);
+	g_option_context_add_main_entries(option_context, entries, NULL);
+	g_option_context_parse(option_context, &argc, &argv, &error);
+	g_option_context_free(option_context);
+
+	if (error) {
+		g_printerr("What did you say?\n %s\n", error->message);
+		g_error_free(error);
+		exit(EXIT_FAILURE);
+	}
+
+	if (!command) {
+		command = command = vte_get_user_shell();
+	}
+
+	if (!command) {
+		command = g_strdup("/bin/sh");
+	}
+	
+	gchar **command_argv = NULL;
+
+	g_shell_parse_argv(command, NULL, &command_argv, &error);
+	g_free(command);
+
+	if (error) {
+		g_printerr("Error parsing command. %s\n", error->message);
+		g_error_free(error);
+		exit(EXIT_FAILURE);
+	}
+
+	VteTerminal *vte = NULL;
+	GtkScrollbar* scrollbar = NULL;
+	GtkBox *box = NULL;
+	GtkWindow *window = NULL;
+
+	GPid child_pid = 0;
 
 	vte = (VteTerminal *) vte_terminal_new();
 	vte_terminal_set_scrollback_lines(vte, -1); //infinite scrollback
@@ -70,12 +108,6 @@ int main (int argc, char *argv[]) {
 	g_signal_connect(vte, "beep", G_CALLBACK(vte_beep_callback), &window);
 	g_signal_connect(vte, "focus-in-event", G_CALLBACK (vte_focus_in_event_callback), &window);
 	g_signal_connect(vte, "key-press-event", G_CALLBACK (vte_key_press_callback), NULL);
-
-	gchar *command = command = vte_get_user_shell();
-	if (command == NULL) {
-		command = g_strdup("/bin/sh");
-	}
-	gchar *command_argv[] = { command, NULL };
 
 	vte_terminal_fork_command_full(
 		(VteTerminal *) vte,
@@ -89,7 +121,7 @@ int main (int argc, char *argv[]) {
 		&child_pid,
 		&error);
 
-	g_free(command);
+	g_strfreev(command_argv);
 
 	if (error) {
 		g_printerr("Error while running user shell: %s\n", error->message);
